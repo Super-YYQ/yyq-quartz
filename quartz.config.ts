e4aa5bb89a9ce11e69373daf7a31cf601b844a1e
yanyaqi <yanyaqi@yonyou.com>
2026-06-02 17:39:34 +0800

    chore: configure Cloudflare Pages domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "zh-CN",
-    baseUrl: "example.com",
+    baseUrl: "yyq-quartz.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
